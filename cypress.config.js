94abf9f941d3ef8dbd2f575148a0692027264b45
Your Name <you@example.com>
2023-06-22 22:34:03 +0100

    Reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
     specPattern: "cypress/e2e/*.feature",
   },
